9bcc3752ecedb3a1c54757a6557a1ba256ab2262
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-15 09:56:27 -0300

    Test - CodeTester created

--- src/test/java/tests/CodeTester.java
+++ src/test/java/tests/CodeTester.java
@@ -1,4 +1,25 @@
 package tests;
 
+import models.Code;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
 public class CodeTester {
+
+    private static final Logger log = Logger.getLogger(Code.class.getName());
+
+    @BeforeAll
+    static void setup(){
+        log.info("Starting test for class Code");
+    }
+
+    @DisplayName("Basic test - Testing getter")
+    @Test
+    void testGetter(){
+        Code code = new Code();
+        log.info("Obtained.: " + code.getCode());
+    }
 }
