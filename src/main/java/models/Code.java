364174f6c010de3f941842c14c4dfd7192e569f9
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-15 09:29:46 -0300

    model Code - created

---
+++ src/main/java/models/Code.java
@@ -0,0 +1,19 @@
+package models;
+
+/**
+ * Class meant to represent the code instance in the game session.
+ * Should randomly generate a code of 4 letter on initialization and return it whenever called.
+ *
+ * @author Nathan (goodguyplayer)
+ * @Version 0
+ * @since 2021-04-15
+ */
+
+/*
+Changelog.:
+
+Version 0.:
+    - class created
+ */
+public class Code {
+}
