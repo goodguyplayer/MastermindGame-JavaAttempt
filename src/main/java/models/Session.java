357301eccbeae62355e5ea2a9e16bf0678d84048
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-15 11:18:51 -0300

    model Session - version 0.1
    Version 0.1.:
        - Added method verifyLetters()
        - Added method printCurrent()
        - Added method resetCounters()
        - Added method isCodeBroken()

--- src/main/java/models/Session.java
+++ src/main/java/models/Session.java
@@ -5,12 +5,18 @@ package models;
  * It should ask the player for an input of 4 letters, compare with the code and loop it until both are correct.
  *
  * @author Nathan (goodguyplayer)
- * @Version 0
+ * @Version 0.1
  * @since 2021-04-15
  */
 
 /*
 Changelog.:
+
+Version 0.1.:
+    - Added method verifyLetters()
+    - Added method printCurrent()
+    - Added method resetCounters()
+    - Added method isCodeBroken()
 
 Version 0.:
     - Class created
@@ -38,27 +44,17 @@ public class Session {
         while (status){
             System.out.println("Please input 4 letters. (Excess letters will be ignored)");
             player.setAttempt();
+            player.increaseScore();
 
-            for (int i = 0; i < 4; i++) {
-                if(checkLetterMatch(i)){
-                    correct++;
-                } else if(checkInCode(i)){
-                    incode++;
-                } else {
-                    wrong++;
-                }
-            }
-            System.out.println("There are " + correct + "  letters that matches the code and are in the right position.");
-            System.out.println("There are " + incode + "  letters that matches the code  but are in the wrong position.");
-            System.out.println("There are " + wrong + "  letters that doesn't match the code.");
+            verifyLetters();
+            printCurrent();
 
-            correct = 0;
-            incode = 0;
-            wrong = 0;
-
-            if (correct == 4){
+            if(isCodeBroken()){
                 status = false;
             }
+
+            resetCounters();
+
         }
         System.out.println("Congratulations, you got the code.");
         System.out.println("Your score was of " + player.getScore());
@@ -90,4 +86,52 @@ public class Session {
         }
         return false;
     }
+
+    /**
+     * Method that verifies the letters based on whether they match, are in the right position or not.
+     * @author Nathan (goodguyplayer)
+     */
+    private void verifyLetters(){
+        for (int i = 0; i < 4; i++) {
+            if(checkLetterMatch(i)){
+                correct++;
+            } else if(checkInCode(i)){
+                incode++;
+            } else {
+                wrong++;
+            }
+        }
+    }
+
+    /**
+     * Method that gets correct, incode and wrong, printing each to the screen
+     * @author Nathan (goodguyplayer)
+     */
+    private void printCurrent(){
+        System.out.println("There are " + correct + "  letters that matches the code and are in the right position.");
+        System.out.println("There are " + incode + "  letters that matches the code  but are in the wrong position.");
+        System.out.println("There are " + wrong + "  letters that doesn't match the code.");
+    }
+
+    /**
+     * Method that reset the counters
+     * @author Nathan (goodguyplayer)
+     */
+    private void resetCounters(){
+        correct = 0;
+        incode = 0;
+        wrong = 0;
+    }
+
+    /**
+     * Method that sees whether correct == 4
+     * @author Nathan (goodguyplayer)
+     */
+    private boolean isCodeBroken(){
+        if (correct == 4){
+            return true;
+        }
+        return false;
+    }
+
 }
