b43f0a6fa5c3ac54a8a13a49f0015db793e398f2
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-15 09:01:35 -0300

    main - Added initial comments

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,17 @@
+/**
+ * This class is purely meant to call the other classes in the system.
+ *
+ * @author Nathan
+ * @Version 0
+ * @since 2021-04-15
+ */
+
+/*
+Changelog.:
+
+Version 0
+    - Class created
+ */
 public class main {
+
 }
