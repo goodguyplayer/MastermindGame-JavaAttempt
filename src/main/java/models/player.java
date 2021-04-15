4a751ba6fafbb0d18d86bb7988b0821025c48d42
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-04-15 09:03:03 -0300

    model player created

---
+++ src/main/java/models/player.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Class meant to represent the player. It should, at minimum, hold the score and attempts.
+ *
+ * @author Nathan
+ * @Version 0
+ * @since 2021-04-15
+ */
+
+/*
+Changelog.:
+
+Version 0
+ */
+public class player {
+}
